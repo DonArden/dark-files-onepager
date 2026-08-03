4b97375b3678e6c20a3f41c98c71c6159ea1b846
Don Arden <donardenwriter@gmail.com>
2026-08-03 17:49:17 +0200

    Text Adjustments + html structure

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/*--- Herpositioneer de pagina bij het laden om te voorkomen dat de gebruiker op een willekeurige positie in de pagina terechtkomt ---*/
+
+if ("scrollRestoration" in history) {
+    history.scrollRestoration = "manual";
+}
+
+window.addEventListener("load", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "instant"
+    });
+});
+
+/*--- Voeg een animatie toe aan de sectietitels wanneer ze in beeld komen ---*/
 const sectionTitles = document.querySelectorAll(".section-title");
 
 const titleObserver = new IntersectionObserver(
